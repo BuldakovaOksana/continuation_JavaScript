2e98a3e098e84ead841dba842519a2dc4233cdfe
«Buldakova <79263143446@mail.ru>
2024-02-06 22:12:36 +0300

    DZ5

---
+++ lesson5/data.js
@@ -0,0 +1,32 @@
+const dataInfo = `
+[
+    {
+        "API":"AdoptAPet",
+        "Description":"Resource to help get pets adopted",
+        "Auth":"apiKey",
+        "Link":"https://www.adoptapet.com/public/apis/pet_list.html",
+        "Category":"Animals"
+    },
+    {
+        "API":"Axolotl",
+        "Description":"Collection of axolotl pictures and facts",
+        "Auth":"",
+        "Link":"https://theaxolotlapi.netlify.app/",
+        "Category":"Animals"
+    },
+    {
+        "API":"Cat Facts",
+        "Description":"Daily cat facts",
+        "Auth":"",
+        "Link":"https://alexwohlbruck.github.io/cat-facts/",
+        "Category":"Animals"
+    },
+    {
+        "API":"Cataas",
+        "Description":"Cat as a service (cats pictures and gifs)",
+        "Auth":"",
+        "Link":"https://cataas.com/",
+        "Category":"Animals"
+    }
+]    
+`
