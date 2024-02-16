5e926c986aed4f570ae0850723cf46e9c666f579
«Buldakova <79263143446@mail.ru>
2024-02-16 22:50:59 +0300

    DZ6

---
+++ lesson6/data.js
@@ -0,0 +1,26 @@
+const dataProducts = `
+[
+    {
+        "id": 1,
+        "img": "img1.jpg",
+        "name": "T-SHIRT",
+        "brand": "MANGO  PEOPLE",
+        "price": 4,
+        "currency": "$",
+        "color": "#red",
+        "size": "XL",
+        "quantity":2	
+    },
+    {
+        "id": 2,
+        "img": "img2.jpg",
+        "name": "CAPSULE",
+        "brand": "ELLERY X M'O",
+        "price": "52",
+        "currency": "$",
+        "color": "#black",
+        "size": "XL",
+        "quantity": 1	
+    }
+]
+`
